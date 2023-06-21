a01a55e435549ccbac005ee69224405d7abbd7a8
Rossi Christian <christian2.rossi@mail.polimi.it>
2023-06-22 00:31:00 +0200

    filters for details and offers

--- Pure HTML/src/main/java/com/christian/rossi/progetto_tiw_2023/Servlets/Filters/DetailsFilter.java
+++ Pure HTML/src/main/java/com/christian/rossi/progetto_tiw_2023/Servlets/Filters/DetailsFilter.java
@@ -5,7 +5,6 @@ import com.christian.rossi.progetto_tiw_2023.Constants.Errors;
 import com.christian.rossi.progetto_tiw_2023.Constants.URLs;
 import com.christian.rossi.progetto_tiw_2023.DAOs.AuctionDAO;
 import com.christian.rossi.progetto_tiw_2023.Utils.PathBuilder;
-import org.apache.commons.lang3.ObjectUtils;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
@@ -16,8 +15,8 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
-@WebFilter(filterName = "OfferFilter", urlPatterns = {URLs.GET_OFFERS_PAGE, URLs.DO_OFFER})
-public class OfferFilter implements Filter {
+@WebFilter(filterName = "OfferFilter", urlPatterns = {URLs.GET_DETAILS_PAGE, URLs.DO_CLOSE})
+public class DetailsFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
@@ -31,12 +30,12 @@ public class OfferFilter implements Filter {
             try {
                 auctionID = Long.valueOf(request.getParameter("details"));
                 List<AuctionBean> list = auctionDAO.getAuctionsByID(auctionID, 1);
-                if (list == null || !list.get(0).isActive() && !list.get(0).getUserID().equals(userID)) {
+                if (list == null || list.get(0).getUserID().equals(userID)) {
                     hresponse.sendRedirect(new PathBuilder(URLs.GET_ERROR_PAGE).addParam("error", Errors.AUCTION_ERROR).addParam("redirect", URLs.GET_HOME_PAGE).toString());
                     return;
                 }
             }
-            catch (NumberFormatException | NullPointerException e) {
+            catch (NumberFormatException e) {
                 hresponse.sendRedirect(new PathBuilder(URLs.GET_ERROR_PAGE).addParam("error", Errors.AUCTION_ERROR).addParam("redirect", URLs.GET_HOME_PAGE).toString());
             }
         } catch (SQLException e) {
