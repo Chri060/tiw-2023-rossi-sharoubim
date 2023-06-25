f51dec5933ba4d10bcf8b7216ed994966fd723e6
Rossi Christian <christian2.rossi@mail.polimi.it>
2023-06-25 23:30:01 +0200

    fixed regex

--- Rich Internet Application/src/main/java/com/christian/rossi/progetto_tiw_2023/Utils/InputChecker.java
+++ Rich Internet Application/src/main/java/com/christian/rossi/progetto_tiw_2023/Utils/InputChecker.java
@@ -6,8 +6,8 @@ public class InputChecker {
     private static final String emailRegex = "[^@ \\t\\r\\n]+@[^@ \\t\\r\\n]+\\.[^@ \\t\\r\\n]+";
     private static final String passwordRegex = "^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,16}$";
     private static final String usernameRegex = "^[a-zA-Z0-9_-]{4,16}$";
-    private static final String cityProvinceRegex = "^[a-zA-Z]{1,32}$";
-    private static final String nameRegex = "^[a-zA-Z ]{1,32}$";
+    private static final String cityProvinceRegex = "^[a-zA-Z ]{1,32}$";
+    private static final String nameRegex = "^[a-zA-Z0-9 ]{1,32}$";
 
     public static boolean checkUsername(String username) { return username.matches(usernameRegex); }
     public static boolean checkEmail(String email) { return email.length() <= 256 && email.matches(emailRegex); }
